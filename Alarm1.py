25db6fbae2269f0a4135f59cc9d1a70930b3efe1
Luuk <luukhendriks13>
2017-10-26 17:06:44 +0200

    Hoi

--- Alarm1.py
+++ Alarm1.py
@@ -2,12 +2,13 @@ import RPi.GPIO as GPIO
 import time
 import pygame
 from socket import *
+import select
 
-host = "192.168.42.2"
+server = "192.168.42.2"
 self = ""
 port = 13000
 buf = 1024
-addr = (host, port)
+addr = (server, port)
 receive_addr = (self, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
 UDPSock.bind(receive_addr)
@@ -22,23 +23,31 @@ GPIO.setup(25, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(16, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 both = 0
-first2 = True
+elevator = 0
+connected = 0
 
 def check_message():
-    if UDPSock.recv != None:
-        (data, addr) = UDPSock.recvfrom(buf)
+    readers,_,_ = select.select([UDPSock],[],[],0.1)
+    for reader in readers:
+        data = reader.recv(buf)
         signal = data.decode('UTF-8')
         if signal == "Reset":
             return True
+        elif signal == "connected":
+            global connected
+            connected = 0
         else:
             return False
-    else:
-        return False
+    return False
+
+def connected_function():
+    data = bytes("connected", 'UTF-8')
+    UDPSock.sendto(data, addr)
 
 while True:
-    if first2 == True:
-        print("uit")
-        first2 = False
+    connected += 1
+    reset = check_message()
+    connected_function()
     GPIO.output(18, GPIO.LOW)
     GPIO.output(23, GPIO.LOW)
     orange = False
@@ -48,35 +57,50 @@ while True:
         both +=1
     else:
         both = 0
-    if both == 50:
+    if both == 50 or connected > 5:
         first = True
         both = 0
         both3 = 0
         while True:
+            connected += 1
+            reset = check_message()
+            connected_function()
             both2 = 0
             GPIO.output(18, GPIO.HIGH)
             GPIO.output(23, GPIO.LOW)
             orange = False
             pygame.mixer.music.stop()
-            if first == True:
-                print("aan")
-                time.sleep(5)
-                first = False
+
+            if first == True and connected < 5:
+                elevator = 0
+                pygame.mixer.music.load("elevator.wav")
+                pygame.mixer.music.set_volume(0.5)
+                pygame.mixer.music.play()
+                while elevator < 100:
+                    connected += 1
+                    reset = check_message()
+                    connected_function()
+                    time.sleep(0.1)
+                    first = False
+                    elevator += 1
+                pygame.mixer.music.stop()
             if GPIO.input(25) and GPIO.input(16):
                 both3 += 1
-            elif GPIO.input(25) == True:
+            elif GPIO.input(25) == True or connected > 5:
                 both3 = 0
                 pygame.mixer.music.stop()
                 pygame.mixer.music.load("beep1.ogg")
-                pygame.mixer.music.set_volume(1.0)
+                pygame.mixer.music.set_volume(0.5)
                 pygame.mixer.music.play(-1)
                 GPIO.output(18, GPIO.LOW)
                 counter = 0
-                while counter < 100:
-                    #reset = check_message()
-                    #if reset == True:
-                        #break
-                    time.sleep(0.1)
+                while counter < 100 and connected < 5:
+                    connected += 1
+                    reset = check_message()
+                    connected_function()
+                    reset = check_message()
+                    if reset == True:
+                        break
                     if counter % 5 == 0:
                         if orange == False:
                             GPIO.output(23, GPIO.HIGH)
@@ -87,21 +111,23 @@ while True:
                     if GPIO.input(16):
                         break
                     counter += 1
-                if counter == 100:
+                if counter == 100 or connected > 5:
                     GPIO.output(23, GPIO.HIGH)
                     orange = True
                     data = bytes("True", 'UTF-8')
                     UDPSock.sendto(data, addr)
                     pygame.mixer.music.stop()
-                    pygame.mixer.music.load("match0.mp3")
-                    pygame.mixer.music.set_volume(0.5)
+                    pygame.mixer.music.load("alarm.wav")
+                    pygame.mixer.music.set_volume(0.1)
                     pygame.mixer.music.play(-1)
                     both2 = 0
                     while True:
-                        #reset = check_message()
-                        #if reset == True:
-                            #first = True
-                            #break
+                        connected += 1
+                        connected_function()
+                        reset = check_message()
+                        if reset == True:
+                            first = True
+                            break
                         if GPIO.input(25) and GPIO.input(16):
                             both2 += 1
                         else:
@@ -110,10 +136,8 @@ while True:
                             first = True
                             pygame.mixer.music.stop()
                             break
-                        time.sleep(0.1)
             if both3 == 50:
                 both3 = 0
-                first2 = True
                 break
             time.sleep(0.1)
 
--- Receiver.py
+++ Receiver.py
@@ -2,19 +2,52 @@
 # Message Receiver
 import RPi.GPIO as GPIO
 from socket import *
-host = ""
+import select
+client1 = "192.168.42.1"
+self = ""
 port = 13000
 buf = 1024
-addr = (host, port)
+receive_addr = (self, port)
+send_addr = (client1, port)
 UDPSock = socket(AF_INET, SOCK_DGRAM)
-UDPSock.bind(addr)
+UDPSock.bind(receive_addr)
 print ("Checking for break-ins...")
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(18, GPIO.OUT)
 GPIO.output(18, GPIO.LOW)
+
+GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #knopje rechts
+GPIO.setup(24, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) #knopje links
+
+connection_counter = 0
+both_buttons = 0
+inbraak = False
+
 while True:
-    (data, addr) = UDPSock.recvfrom(buf)
-    signal = data.decode('UTF-8')
-    if signal == "True":
+    data = bytes("connected", 'UTF-8')
+    UDPSock.sendto(data, send_addr)
+    if connection_counter == 5:
         GPIO.output(18, GPIO.HIGH)
+        connection_counter = 0
+    readers, _, _ = select.select([UDPSock], [], [], 0.1)
+    for reader in readers:
+        data = reader.recv(buf)
+        signal = data.decode('UTF-8')
+        if signal == "True":
+            GPIO.output(18, GPIO.HIGH)
+            inbraak = True
+        if signal == "connected" and inbraak == False:
+            connection_counter = 0
+            GPIO.output(18, GPIO.LOW)
+            continue
+    if GPIO.input(23) and GPIO.input(24):
+        both_buttons += 1
+    else:
+        both_buttons = 0
+    if both_buttons == 50:
+        data = bytes("Reset", 'UTF-8')
+        UDPSock.sendto(data, send_addr)
+        GPIO.output(18, GPIO.LOW)
+        inbraak = False
+    connection_counter += 1
